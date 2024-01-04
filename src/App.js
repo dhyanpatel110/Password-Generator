7c1ec28c9f865d99257d76398d41ad37eb5924fd
DHYAN PATEL <dhyanpatel110@gmail.com>
2024-01-04 22:31:22 +0530

    speed-insights package added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Layout from "./components/Layout"
 import Generator from "./components/Generator"
 import { SettingsProvider } from "./contexts/SettingsContext"
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 export default function App() {
   return (
     <SettingsProvider>
       <Layout>
         <Generator />
+        <SpeedInsights />
       </Layout>
     </SettingsProvider>
   )
